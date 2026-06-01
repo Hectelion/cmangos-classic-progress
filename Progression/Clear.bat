13873feceadbd2db0a8e20b8d576079a28b38dcf
Hectelion <antonio.mezzetti@gmail.com>
2026-06-01 17:03:50 +0200

    Add files via upload

---
+++ Progression/Clear.bat
@@ -0,0 +1,38 @@
+@echo off
+
+del .\DataSaved\*.*
+del .\patch_1\patch*.*
+del .\patch_2\patch*.*
+del .\patch_3\patch*.*
+del .\patch_4\patch*.*
+del .\patch_5\patch*.*
+del .\patch_6\patch*.*
+del .\patch_7\patch*.*
+del .\patch_8\patch*.*
+del .\patch_9\patch*.*
+del .\patch_10\patch*.*
+del .\patch_11\patch*.*
+del .\patch_12\patch*.*
+del .\patch_13\patch*.*
+del .\patch_14\patch*.*
+del .\patch_15\patch*.*
+del .\patch_16\patch*.*
+del .\patch_17\patch*.*
+del .\patch_18\patch*.*
+del .\patch_19\patch*.*
+del .\patch_20\patch*.*
+del .\patch_21\patch*.*
+del .\patch_22\patch*.*
+del .\patch_23\patch*.*
+del .\patch_24\patch*.*
+del .\patch_25\patch*.*
+del .\patch_26\patch*.*
+del .\patch_27\patch*.*
+del .\patch_28\patch*.*
+del .\patch_29\patch*.*
+del .\patch_30\patch*.*
+del .\patch_31\patch*.*
+del .\patch_32\patch*.*
+
+pause
+
